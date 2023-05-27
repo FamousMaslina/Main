c8a11f98714714244bbbffbc5f7fa18549cc25c3
FamousMaslina <famousmaslina@gmail.com>
2023-05-27 21:59:22 +0300

    FINAL ITCH.IO

--- folder/shooter/player.py
+++ folder/shooter/player.py
@@ -5,6 +5,7 @@ import time as tm
 from russia import *
 ax = 200
 ay = 600
+
 cooldown = None
 count = 0
 class Character(sprite.Sprite): #in the parentheses it is indicated that the class inherits from the Sprite class
@@ -37,30 +38,76 @@ class Character(sprite.Sprite): #in the parentheses it is indicated that the cla
          self.rect.x += 5
       if keys[K_SPACE]:
          self.shoot()
+         sfx = mixer.Sound('assets/shooter/bullet2.mp3')
+         sfx.play(0)
+
       self.bullets.update()
       self.bullets.draw(screen) 
+         
       for bullet in self.bullets:
          if bullet.getY() < -100:
               bullet.kill()
          elif sprite.collide_rect(bullet, russia):
             print("russia ")
             russia.kill()
+            russia.rect = Rect(0, 0, 0, 0)
             bullet.kill()
          elif sprite.collide_rect(bullet, russia2):
             print("russia 2")
             russia2.kill()
+            russia12.rect = Rect(0, 0, 0, 0)
             bullet.kill()
          elif sprite.collide_rect(bullet, russia3):
             print("russia 3")
             russia3.kill()
+            russia3.rect = Rect(0, 0, 0, 0)
             bullet.kill()
          elif sprite.collide_rect(bullet, russia4):
             print("russia 4")
             russia4.kill()
+            russia4.rect = Rect(0, 0, 0, 0)
             bullet.kill()
          elif sprite.collide_rect(bullet, russia5):
             russia5.kill()
+            russia5.rect = Rect(0, 0, 0, 0)
             print("russia 5")
+            bullet.kill()
+
+         elif sprite.collide_rect(bullet, russia6):
+            print("russia 6")
+            russia6.kill()
+            russia6.rect = Rect(0, 0, 0, 0)
+            bullet.kill()
+         elif sprite.collide_rect(bullet, russia7):
+            print("russia 7")
+            russia7.kill()
+            russia7.rect = Rect(0, 0, 0, 0)
+            bullet.kill()
+         elif sprite.collide_rect(bullet, russia8):
+            print("russia 8")
+            russia8.kill()
+            russia8.rect = Rect(0, 0, 0, 0)
+            bullet.kill()
+         elif sprite.collide_rect(bullet, russia9):
+            print("russia 9")
+            russia9.kill()
+            russia9.rect = Rect(0, 0, 0, 0)
+            bullet.kill()
+         elif sprite.collide_rect(bullet, russia10):
+            russia10.kill()
+            russia10.rect = Rect(0, 0, 0, 0)
+            print("russia 10")
+            bullet.kill()
+
+         elif sprite.collide_rect(bullet, russia11):
+            print("russia 11")
+            russia11.kill()
+            russia11.rect = Rect(0, 0, 0, 0)
+            bullet.kill()
+         elif sprite.collide_rect(bullet, russia12):
+            russia12.kill()
+            russia12.rect = Rect(0, 0, 0, 0)
+            print("russia 12")
             bullet.kill()
 
    def reset_position(self):
--- folder/shooter/shooter.py
+++ folder/shooter/shooter.py
@@ -6,7 +6,9 @@ from player import *
 import pygame as pyg
 
 
+clock = time.Clock()
 
+FPS = 60
 init()
 screen = display.set_mode((400, 700))
 clock = pyg.time.Clock()
@@ -21,10 +23,18 @@ characters.add(russia2)
 characters.add(russia3)
 characters.add(russia4)
 characters.add(russia5)
+characters.add(russia6)
+characters.add(russia7)
+characters.add(russia8)
+characters.add(russia9)
+characters.add(russia10)
+characters.add(russia11)
+characters.add(russia12)
 characters.add(f16)
 
 
-    
+mixer.music.load('assets/shooter/theme.mp3')
+mixer.music.play(-1)
 
 
 cooldown = 0
@@ -34,7 +44,7 @@ while running:
         if event.type == QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+    screen.fill((135, 206, 235))
     #f16.update()
     characters.update(screen)
 
@@ -43,10 +53,11 @@ while running:
     #if sprite.collide_rect(Bullet, russia):
         #count += 1
         #print(count)
-    mixer.music.load('assets/shooter/theme.mp3')
-    mixer.music.play(-1)
+
     display.flip()
     display.update()
     dt = clock.tick(60) / 1000
-
+    #delay = 2 
+    #delay_ticks = int(delay * FPS)  
+    #time.delay(delay_ticks)
 quit()
